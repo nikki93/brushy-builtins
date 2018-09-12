0d7e5b379d0b532538f74c2baf7eda87cf0b76b6
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-09-12 15:42:06 -0700

    apply radius to 'image', bigger line size

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ effect.glow.min_luma = 0
 brush.settingsShape = {
     text = { type = 'text' },
     image = { type = 'image' },
-    radius = { type = 'number', style = 'slider', min = 0, max = 30 },
+    radius = { type = 'number', style = 'slider', min = 0, max = 1 },
     color = { type = 'color' },
 }
 
@@ -19,7 +19,7 @@ brush.settingsShape = {
 function brush.paint(x, y, dx, dy)
     effect(function()
         if brush.settings.radius then
-            love.graphics.setLineWidth(brush.settings.radius)
+            love.graphics.setLineWidth(120 * brush.settings.radius)
         end
         if brush.settings.color then
             local color = brush.settings.color
@@ -37,6 +37,9 @@ function brush.paint(x, y, dx, dy)
         if image then
             local w, h = image:getDimensions()
             local scale = love.graphics.getWidth() / w
+            if brush.settings.radius and brush.settings.radius > 0 then
+                scale = brush.settings.radius * scale
+            end
             love.graphics.draw(image, x, y, 0, scale, scale, 0.5 * w, 0.5 * h)
         end
     end)
